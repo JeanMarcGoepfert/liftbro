43d57319b5f51334a121729cd5d6ed9c36478060
Jean-Marc Goepfert <goepfertjeanmarc@gmail.com>
2014-11-12 22:53:52 +1100

    added exercises to home page stats

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -1,18 +1,32 @@
 'use strict';
 
 angular.module('liftbroApp')
-  .controller('DashboardCtrl', function ($scope, $state, Auth, Workouts) {
+  .controller('DashboardCtrl', function ($scope, $state, Auth, Workouts, Exercises) {
     $scope.workouts = {
+      list: [],
+    };
+    $scope.exercises = {
       list: [],
     };
 
     Workouts.index(6)
     .then(function(data) {
       $scope.workouts.list = data;
+      getExercises();
     }, function(err) {
       //todo handle error
       console.log(err);
     });
+
+    function getExercises() {
+      Exercises.index()
+      .then(function(data) {
+        $scope.exercises.list = data;
+      }, function(err) {
+        //todo handle error
+        console.log(err);
+      });
+    }
 
     $scope.viewWorkout = function(workout) {
       Workouts.navigateToWorkout(workout);
